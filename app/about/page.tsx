fcf81b58152f5c74c0f1c7cb210ab45d87e20ccf
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-12-26 14:01:08 -0700

    metatags

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Page',
+  description: 'This is the about page',
+  keywords: 'about, page',
+}
+
 export default function AboutPage() {
   return (
     <>
