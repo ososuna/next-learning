83c9db8003615f6b855b38b161255996385b342c
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-01-02 14:58:39 -0700

    learning client and server components with counter

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from 'next';
+import { CartCounter } from '@/app/shopping-cart';
+
+export const metadata: Metadata = {
+  title: 'Shopping cart',
+  description: 'Shopping cart products page',
+};
+
 export default function CounterPage() {
+
   return (
-    <>
-      <h1>Counter</h1>
-    </>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Shopping cart products</span>
+      <CartCounter value={ 10 } />
+    </div>
   )
 }
