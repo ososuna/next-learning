4f632acb049811bb97f41534126754d4ccc33296
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-01-02 17:22:22 -0700

    fetching pokemons

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5';
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5';
 import { SidebarMenuItem } from './SidebarMenuItem';
 
 const menuItems = [
@@ -15,11 +15,17 @@ const menuItems = [
     title: 'Counter',
     subtitle: 'Contador Client Side'
   },
+  {
+    path: '/dashboard/pokemons',
+    icon: <IoFootball size={ 24 } />,
+    title: 'Pokemons',
+    subtitle: 'Static Generation'
+  },
 ];
 
 export const Sidebar = () => {
   return (
-    <div id="menu" style={{ width: '400px' }} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+    <div id="menu" style={{ width: '400px' }} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
           <IoLogoReact className="mr-2"></IoLogoReact>
