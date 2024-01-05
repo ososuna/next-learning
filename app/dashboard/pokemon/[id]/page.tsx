e929e8cae819549124b38af3d7c2c4156ab3df91
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-01-04 19:06:38 -0700

    pokemon page url arguments

---
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  params: { id: string };
+}
+export default function PokemonPage({ params }: Props) {
+  return (
+    <div>
+      <h1>Pokemon { params.id }</h1>
+    </div>
+  );
+}
