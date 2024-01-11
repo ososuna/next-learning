3080512a1d11245139efb44504dece7f2e4a84b1
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-01-10 20:46:05 -0600

    get pokemon by id

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,19 @@
+import { Pokemon } from '@/pokemons';
+
 interface Props {
   params: { id: string };
 }
-export default function PokemonPage({ params }: Props) {
+
+const getPokemon = async(id: string): Promise<Pokemon> => {
+  return await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache'
+  }).then(res => res.json());
+}
+
+export default async function PokemonPage({ params }: Props) {
+
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
       <h1>Pokemon { params.id }</h1>
--- pokemons/index.ts
+++ pokemons/index.ts
@@ -1,3 +1,4 @@
+export type { Pokemon } from './interfaces/pokemon';
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
 export { PokemonGrid } from './components/PokemonGrid';
