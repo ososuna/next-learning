83e93783da434fc41bbe4ba3c496ae807aed95f6
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-01-10 20:52:16 -0600

    dynamic metadata

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,26 @@
+import { Metadata } from 'next';
 import { Pokemon } from '@/pokemons';
 
 interface Props {
   params: { id: string };
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+
+  const { id, name } = await getPokemon(params.id);
+
+  return {
+    title: `#${id} - ${name}`,
+    description: `Pokemon ${params.id} description`,
+  }
+}
+
 const getPokemon = async(id: string): Promise<Pokemon> => {
   return await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-    cache: 'force-cache'
+    cache: 'force-cache',
+    // next: {
+    //   revalidate: 60 * 60 * 24,
+    // }
   }).then(res => res.json());
 }
 
@@ -16,7 +30,7 @@ export default async function PokemonPage({ params }: Props) {
 
   return (
     <div>
-      <h1>Pokemon { params.id }</h1>
+      <h1>Pokemon { pokemon.name }</h1>
     </div>
   );
 }
